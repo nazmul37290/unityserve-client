149cf49d24a6be23d7eca841517889c411baa527
nazmul37290 <nazmulnjm@gmail.com>
2024-05-12 11:43:58 +0600

    loaded data from the database for need now section

--- src/components/VolunteerCard.jsx
+++ src/components/VolunteerCard.jsx
@@ -1,27 +1,39 @@
 import { Link } from "react-router-dom";
 import bannerImg from "../assets/banner.jpeg";
-const VolunteerCard = () => {
+const VolunteerCard = ({ post }) => {
+  const {
+    category,
+    deadline,
+    description,
+    location,
+    thumbnail,
+    organizer,
+    title,
+    volunteersNeeded,
+  } = post;
   return (
     <div className="relative h-[250px] w-[350px] mb-24">
       <div className="h-[250px] w-[350px]  relative border-4 border-gray-300 rounded-3xl">
-        <img src={bannerImg} className="w-full h-full rounded-3xl" alt="" />
+        <img src={thumbnail} className="w-full h-full rounded-3xl" alt="" />
       </div>
       <div className="h-[200px] absolute bg-white left-1/2 -translate-x-1/2 -bottom-[100px] w-[300px] hover:scale-110 shadow-lg hover:text-white rounded-3xl duration-1000">
         <div className=" p-4 md:p-4 ">
           <h1 className="text-xl font-bold text-gray-800 dark:text-white">
-            Post Title
+            {title}
           </h1>
 
           <p className="mt-2 text-sm text-gray-600  dark:text-gray-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit In odit
+            {description}
           </p>
 
           <div className="flex mt-2 item-center gap-4">
-            <h1 className="text-sm font-bold text-gray-700 dark:text-gray-200 ">
-              Category
+            <h1
+              className={`bg-main rounded-r-md text-white py-1 px-2 text-sm font-bold  dark:text-gray-200 `}
+            >
+              {category}
             </h1>
             <h1 className="text-sm font-bold text-gray-700 dark:text-gray-200 md:text-sm">
-              Deadline
+              Deadline: <span>{deadline}</span>
             </h1>
           </div>
 
--- src/sections/VolunteerNeedNow.jsx
+++ src/sections/VolunteerNeedNow.jsx
@@ -2,32 +2,29 @@ import { Link } from "react-router-dom";
 import VolunteerCard from "../components/VolunteerCard";
 
 import { FaArrowRight } from "react-icons/fa6";
+import axios from "axios";
+import { useState } from "react";
 
 const VolunteerNeedNow = () => {
+  const [posts, setPosts] = useState([]);
+  useState(() => {
+    axios(`${import.meta.env.VITE_URL}/posts`).then((result) => {
+      console.log(result.data);
+      setPosts(result.data);
+    });
+  }, []);
   return (
     <div className="bg-[#91C591] p-10">
       <h1 className="text-xl lg:text-3xl font-bold text-center max-w-fit mx-auto pb-2 border-b-light border-b-2">
         Volunteer Need Now
       </h1>
       <div className="max-w-7xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mx-auto mt-16">
-        <div className="flex justify-center items-center">
-          <VolunteerCard></VolunteerCard>
-        </div>
-        <div className="flex justify-center items-center">
-          <VolunteerCard></VolunteerCard>
-        </div>
-        <div className="flex justify-center items-center">
-          <VolunteerCard></VolunteerCard>
-        </div>
-        <div className="flex justify-center items-center">
-          <VolunteerCard></VolunteerCard>
-        </div>
-        <div className="flex justify-center items-center">
-          <VolunteerCard></VolunteerCard>
-        </div>
-        <div className="flex justify-center items-center">
-          <VolunteerCard></VolunteerCard>
-        </div>
+        {posts.map((post) => {
+          return <VolunteerCard key={post._id} post={post}></VolunteerCard>;
+          // <div className="flex justify-center items-center">
+          //
+          // </div>
+        })}
       </div>
       <div className="flex justify-center mt-16">
         <Link>
